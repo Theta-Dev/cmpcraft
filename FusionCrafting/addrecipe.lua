335863af5c2645eb0451cb56719b0e2594d8f538
Theta-Dev <t.testboy@gmail.com>
2020-05-10 21:01:46 +0200

    updated

--- FusionCrafting/addrecipe.lua
+++ FusionCrafting/addrecipe.lua
@@ -1,8 +1,23 @@
-for i=1, 17, 1 do
+recipe = ""
+count = 0
+
+for i=1, 16, 1 do
+	turtle.select(i)
 	if turtle.getItemCount() > 0 then
 		item = turtle.getItemDetail()
-		line = item.count .. ':' .. item.name .. ':' .. item.damage .. ';'
+		line = item.count .. ":" .. item.name .. ":" .. item.damage .. ";"
 
 		print(line)
+		recipe = recipe .. line
+		count = count + 1
 	end
+end
+
+if count > 1 then
+	file = fs.open("recipes.csv", "a")
+	file.writeLine(recipe)
+	file.close()
+	print("Recipe added")
+else
+	print("Less than 2 items found")
 end
