24b48bf5b3e7c2c95ce480ef2bc8d07843355e84
Theta-Dev <t.testboy@gmail.com>
2020-05-10 22:35:23 +0200

    added rotating

--- FusionCrafting/fusionCrafting.lua
+++ FusionCrafting/fusionCrafting.lua
@@ -5,7 +5,7 @@ BLOCK_CHEST = "minecraft:chest"
 posX = 0
 posY = 0
 posZ = 0
-rotation = 2
+rt = 2
 
 function home()
     function moveHome()
@@ -14,7 +14,7 @@ function home()
         _, block = turtle.inspectDown()
         if block.name == BLOCK_HOME then return
         elseif block.name == BLOCK_FIELD then
-            for i=0, 3, 1 do
+            for i=1, 5, 1 do
                 if not turtle.forward() then
                     turtle.turnRight()
                 end
@@ -32,7 +32,7 @@ function home()
     end
 
     function rotateHome()
-        for i=0, 3, 1 do
+        for i=1, 4, 1 do
             _, block = turtle.inspect()
             if block.name == BLOCK_CHEST then return
             else
@@ -48,11 +48,27 @@ function home()
     posX = 0
     posY = 0
     posZ = 0
-    rotation = 2
+    rt = 2
 end
 
-function move(x, y, z)
+function rotate(r)
+    delta = r - rt
+    if delta == 0 then return
+    elseif Math.abs(delta) == 3 then
+        if delta > 0 then turtle.turnLeft()
+        else turtle.turnRight() end
+    else
+        for i=1, Math.abs(delta), 1 do
+            if delta > 0 then turtle.turnRight()
+            else turtle.turnLeft() end
+        end
+    end
+end
 
+function move(x, y, z, r)
+    function moveX()
+
+    end
 end
 
 function refuel()
